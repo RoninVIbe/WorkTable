1969750e9af6c3ecff09b28492b751dd93b6d4c0
RoninVIbe <166244017+RoninVIbe@users.noreply.github.com>
2025-05-01 23:40:53 +0500

    Delete rrr directory

--- rrr/settings.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96c5333466bbd500a7ae42f38cffd92d47d75a1f21dbd1a16e3e3aa4a2bf6cfb
-size 3588
--- rrr/urls.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b0b21db658fc59d565a2b142185ea7f833970f48eb448000d83afc0477258fc
-size 1093
